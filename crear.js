73e5077479a359464f57214f7cbeb3a0decb641c
aganbake <abdul.villarroel@gmail.com>
2023-06-22 22:54:13 -0400

    cambio

--- crear.js
+++ crear.js
@@ -1,13 +1,36 @@
 const fs = require("fs");
 
-let data = `
-A la fecha: ${fecha}
+// parte 1
+const arg = process.argv.slice(2);
+let nombre = String(arg[0]);
+let ext = String(arg[1]);
+let tipo = String(arg[2]);
+let monto = Number(arg[3]);
+let resultado = 0;
+// node index.js cambio txt dolar 25000
+
+// process.on("message", (message) => {
+//   resultado = calcular(message.number);
+//   console.log(resultado);
+//   process.send(resultado);
+//   process.exit();
+// });
+
+// function calcular(number) {
+//   resultado = monto / number;
+//   return resultado;
+// }
+
+resultado = monto / dolar;
+
+let template = `
+A la fecha: por aca
 Fue realizada cotización con los siguientes datos:
-Cantidad de pesos a convertir: ${pesos} pesos
-Convertido a ${moneda} da un total de:
-${resultados}
+Cantidad de pesos a convertir: ${monto} pesos
+Convertido a ${tipo} da un total de:
+${resultado}
 `;
 
-fs.writeFile("cambio.txt", data, "utf-8", () => {
+fs.writeFile(`${nombre}.${ext}`, template, "utf-8", () => {
   console.log("Archivo creado con éxito");
 });
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const https = require("https");
-const fs = require("fs");
-const express = require("express");
+const { spawn } = require("child_process");
 
-const app = express();
-app.use(express.static(__dirname + "/public"));
-
-const PORT = 3000;
 const API_URL = "https://mindicador.cl/api";
 
-// localhost:3000
+// parte 1
+const arg = process.argv.slice(2);
+let nombre = String(arg[0]);
+let ext = String(arg[1]);
+let tipo = String(arg[2]);
+let monto = Number(arg[3]);
+// node index.js cambio txt dolar 25000
+
+// parte 2
+
+let uf,
+  ivp,
+  dolar,
+  dolar_intercambio,
+  euro,
+  ipc,
+  imacec,
+  tpm,
+  libra_cobre,
+  tasa_desempleo,
+  bitcoin = 0;
 
 https
   .get(API_URL, (resp) => {
@@ -17,20 +32,39 @@ https
       data += chunk;
       //   console.log("Chunk: " + chunk);
     });
+
     resp.on("end", () => {
       //   console.log("data: ", JSON.parse(data));
       respuesta = JSON.parse(data);
-      console.log(respuesta.uf.valor);
+      //   console.log(respuesta.uf.valor);
+      uf = respuesta.uf.valor;
+      ivp = respuesta.ivp.valor;
+      dolar = respuesta.dolar.valor;
+      dolar_intercambio = respuesta.dolar_intercambio.valor;
+      euro = respuesta.euro.valor;
+      imacec = respuesta.imacec.valor;
+      ipc = respuesta.ipc.valor;
+      tpm = respuesta.tpm.valor;
+      libra_cobre = respuesta.libra_cobre.valor;
+      tasa_desempleo = respuesta.tasa_desempleo.valor;
+      bitcoin = respuesta.bitcoin.valor;
     });
   })
   .on("error", (err) => {
     console.log("Error: " + err.message);
   });
 
-app.get("/", (req, res) => {
-  res.send("index.html");
+// Parte 3
+
+// const childProcess = fork("./crear.js");
+// childProcess.send({ number: dolar });
+
+const child = spawn("node", ["crear.js", nombre, ext, tipo, monto, dolar]);
+
+child.stdout.on("data", (data) => {
+  console.log(`El contenido del archivo es:\n\n${data}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto: ${PORT}`);
+child.stderr.on("data", (data) => {
+  console.error(`Error: ${data}`);
 });
