09f4e11b4721c0331d08ed2f2406edf122c83132
aganbake <abdul.villarroel@gmail.com>
2023-06-23 00:02:33 -0400

    latest release

--- crear.js
+++ crear.js
@@ -6,25 +6,11 @@ let nombre = String(arg[0]);
 let ext = String(arg[1]);
 let tipo = String(arg[2]);
 let monto = Number(arg[3]);
-let resultado = 0;
-// node index.js cambio txt dolar 25000
-
-// process.on("message", (message) => {
-//   resultado = calcular(message.number);
-//   console.log(resultado);
-//   process.send(resultado);
-//   process.exit();
-// });
-
-// function calcular(number) {
-//   resultado = monto / number;
-//   return resultado;
-// }
-
-resultado = monto / dolar;
+let resultado = Number(arg[4]);
+let fecha = Date(arg[5]);
 
 let template = `
-A la fecha: por aca
+A la fecha: ${fecha}
 Fue realizada cotización con los siguientes datos:
 Cantidad de pesos a convertir: ${monto} pesos
 Convertido a ${tipo} da un total de:
@@ -32,5 +18,5 @@ ${resultado}
 `;
 
 fs.writeFile(`${nombre}.${ext}`, template, "utf-8", () => {
-  console.log("Archivo creado con éxito");
+  console.log("Archivo creado con éxito: ", template);
 });
